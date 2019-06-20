9e7734cc28511b37ec74445c44d1e08bed6331ed
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-20 14:13:05 +0000

    feature: Try out a simple logged in check as explained in class

--- friends/src/components/Container.js
+++ friends/src/components/Container.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Redirect } from 'react-router-dom';
 
 import LoginForm from './LoginForm';
 import FriendsList from './FriendsList';
@@ -6,8 +7,20 @@ import FriendsList from './FriendsList';
 export default function Container() {
   return (
     <div>
-      <LoginForm/>
-      <FriendsList />
+      <Route
+        exact
+        path="/"
+        render={() => {
+          if (localStorage.getItem('token')) {
+            return (
+              <FriendsList />
+            )
+          }
+          return <Redirect to='login' />
+        }}
+      />
+
+      <Route path='/login' component={LoginForm} />
     </div>
   )
 }

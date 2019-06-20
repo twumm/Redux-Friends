3ced3b6bd355389b2fb525ec35ab65e3732d188c
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-20 12:40:40 +0000

    feature: Create initial friends state as an empty object Import action types Create friendsReducer for 4 different actions Combine the reducer and export

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,30 @@
+import { combineReducers } from 'redux';
+import * as types from '../actions';
+
+const initialState = {
+  friends: [],
+}
+
+export function friendsReducer(state = initialState.friends, action) {
+  switch(action.type) {
+    case (types.ADD_FRIENDS):
+      return action.payload;
+    case (types.ADD_FRIEND):
+      return [...state, action.payload];
+    case (types.UPDATE_FRIEND):
+        return state.map(friend => {
+          if (friend.id === action.payload.id) {
+            return [ ...friend, action.payload ];
+          }
+          return friend;
+        })
+    case (types.DELETE_FRIEND):
+      return state.filter(friend => friend.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+export const combinedReducers = combineReducers({
+  friends: friendsReducer,
+});

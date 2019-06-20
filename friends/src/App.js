0c5fe8c7575b86c4e92663c6bcc330e544fa7a70
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-20 12:42:12 +0000

    feature: Import needed dependencies Create the redux store Wrap the application with the redux Provider, passing store as a prop

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import { combinedReducers } from './reducers'
 import './App.css';
+
+const store = createStore(
+  combinedReducers,
+  {},
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        
+      </div>
+    </Provider>
   );
 }
 

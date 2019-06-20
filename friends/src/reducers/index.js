f5a751fa7049d5a3f18b55e81959b95751adec62
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-20 17:57:34 +0000

    feature: Add other reducers

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -2,29 +2,78 @@ import { combineReducers } from 'redux';
 import * as types from '../actions';
 
 const initialState = {
+  deletingFriend: false,
+  fetchingFriends: false,
   friends: [],
+  loggingIn: false,
+  savingFriends: false,
+  updatingFriend: false,
+  error: null
 }
 
-export function friendsReducer(state = initialState.friends, action) {
+export function friendsReducer(state = initialState, action) {
   switch(action.type) {
     case (types.GET_FRIENDS):
-      return action.payload;
+      return { ...state, friends: action.payload };
+
     case (types.ADD_FRIEND):
-      return [...state, action.payload];
+      return {
+        ...state,
+        friends: [ ...state.friends, action.payload ]
+      };
+
     case (types.UPDATE_FRIEND):
-        return state.map(friend => {
+        return state.friends.map(friend => {
           if (friend.id === action.payload.id) {
             return [ ...friend, action.payload ];
           }
           return friend;
         })
+
     case (types.DELETE_FRIEND):
-      return state.filter(friend => friend.id !== action.payload);
+      return state.friends.filter(friend => friend.id !== action.payload);
+
+    case (types.DELETING_FRIEND):
+      return {
+        ...state,
+        deletingFriend: action.payload
+      }
+    
+    case (types.FETCHING_FRIEND):
+      return {
+        ...state,
+        fetchingFriend: action.payload
+      }
+      
+    case (types.LOGGING_IN):
+      return {
+        ...state,
+        loggingIn: action.payload
+      }
+
+    case (types.SAVING_FRIENDS):
+      return {
+        ...state,
+        savingFriends: action.payload
+      }
+
+    case (types.UPDATING_FRIEND):
+      return {
+        ...state,
+        updatingFriend: action.payload
+      }
+
+    case (types.ERROR):
+      return {
+        ...state,
+        error: action.payload
+      }
+
     default:
       return state;
   }
 }
 
 export const combinedReducers = combineReducers({
-  friends: friendsReducer,
+  friendsReducer,
 });

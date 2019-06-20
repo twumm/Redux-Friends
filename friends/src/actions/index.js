8e6f9f37edb43d943d602ddc958ecd5c46fbf3b2
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-20 12:54:47 +0000

    feature: Create action creators for adding friends, adding friend, updating friend and deleting friend

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,4 +1,44 @@
+import axios from 'axios';
+import { types } from 'util';
+
 export const ADD_FRIENDS = 'ADD_FRIENDS';
 export const ADD_FRIEND = 'ADD_FRIEND';
 export const UPDATE_FRIEND = 'UPDATE_FRIEND';
 export const DELETE_FRIEND = 'UPDATE_FRIEND';
+
+export function addFriends(friends) {
+  return {
+    type: ADD_FRIENDS,
+    payload: friends,
+  }
+} 
+
+export function addFriend(name, age, email) {
+  return {
+    type: ADD_FRIEND,
+    payload: {
+      name,
+      age,
+      email,
+    }
+  }
+}
+
+export function updateFriend(id, name, age, email) {
+  return {
+    type: UPDATE_FRIEND,
+    payload: {
+      id,
+      name,
+      age,
+      email,
+    }
+  }
+}
+
+export function deleteFriend(id) {
+  return {
+    type: DELETE_FRIEND,
+    payload: id,
+  }
+}
